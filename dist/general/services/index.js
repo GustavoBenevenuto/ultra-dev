7d80089339ddcfbd98044edd62413721e66fc3f9
Gustavo Benevenuto <gustavo.benesilva@hotmail.com>
2023-10-06 20:57:20 -0300

    Test

---
+++ dist/general/services/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PalindromeService = void 0;
+var PalindromeService_1 = require("./PalindromeService");
+Object.defineProperty(exports, "PalindromeService", { enumerable: true, get: function () { return PalindromeService_1.PalindromeService; } });
